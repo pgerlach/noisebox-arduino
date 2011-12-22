f560afb2e647142c220ff9975be85116fced00f4
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2011-12-22 19:35:25 +0100

    add rfid reader code

--- noisebox_arduino/noisebox_arduino.ino
+++ noisebox_arduino/noisebox_arduino.ino
@@ -15,8 +15,16 @@ struct {
   { 6, "vplus", LOW },
 };
 
+// buttons
 int buttonState = 0;
 int i;
+
+// rfid
+int  val = 0; 
+char code[10];
+char csum[2];
+int bytesread = 0; 
+
 
 void setup() {
   for (i=0; i<nbButtons; ++i) {
@@ -43,6 +51,8 @@ int debounce(int pin) {
 }
 
 void loop(){
+  
+  // buttons
   int i;
   for (i=0; i<nbButtons; ++i) {
     buttonState = debounce(tab[i].pin);
@@ -51,5 +61,37 @@ void loop(){
     }
     tab[i].previousState = buttonState;
   }
+  
+  // rfid ( datasheet at http://www.seeedstudio.com/wiki/index.php?title=Electronic_brick_-_125Khz_RFID_Card_Reader)
+  if (Serial.available() > 0)
+  {
+    if((val = Serial.read()) == 0x02) {
+      bytesread = 0; 
+      while(bytesread<10) {              // read 10 digit code 
+        if( Serial.available() > 0) { 
+          code[bytesread++] = Serial.read();         // add the digit           
+        }
+      }
+      bytesread = 0;
+      while (bytesread < 2) {
+        if (Serial.available() > 0) {
+          csum[bytesread++] = Serial.read();
+        }
+        // TODO check the checksum
+      }
+      bytesread = 0;
+      while (bytesread < 1) {
+        if (Serial.available() > 0) {
+          val = Serial.read();
+          bytesread++;
+          if (val == 0x03) {
+            Serial.print("rfid "); Serial.println(code);
+          }
+        }
+      }
+      bytesread = 0;
+    }
+  }
+
 }
 
